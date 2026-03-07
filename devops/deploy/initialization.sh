379113d19cef3dccb411c96999c69b2282fda626
jason <544094478@qq.com>
2026-03-07 04:37:28 +0000

    deploy

--- devops/deploy/initialization.sh
+++ devops/deploy/initialization.sh
@@ -13,7 +13,15 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 kind load docker-image mock:1.0 --name mock
 
 
-
+# apply resources
+kubectl apply -f ./devops/deploy/k8s/mock_namespace.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_config.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_secret.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_role.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_service_account.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_role_binding.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_deployment_inject_env.yaml
+kubectl apply -f ./devops/deploy/k8s/mock_ingress.yaml
 
 
 
