500954a1c4abf4596f3865534a157777c340a947
Jason-fy-wang <544094478@qq.com>
2026-03-07 13:47:53 +0000

    helm deploy

---
+++ devops/helm/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# create kind cluster
+kind create cluster --name mock --config=./devops/deploy/kind-config.yaml
+
+# add controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+# load image into kind cluster
+kind load docker-image mock:1.0 --name mock
