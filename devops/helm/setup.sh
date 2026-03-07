780ece30c6d59b3215705887134cd02b1011b381
Jason-fy-wang <544094478@qq.com>
2026-03-07 14:12:27 +0000

    setup

--- devops/helm/setup.sh
+++ devops/helm/setup.sh
@@ -8,3 +8,6 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 
 # load image into kind cluster
 kind load docker-image mock:1.0 --name mock
+
+# install chart
+helm install mock mockapi -f ./devops/helm/mockapi/values.yaml
